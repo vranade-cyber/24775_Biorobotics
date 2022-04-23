08a93d12d48da6b7ad7a908c1ae99bc0591f27a3
vranade-cyber <94488661+vranade-cyber@users.noreply.github.com>
2022-04-22 22:15:44 -0400

    Full_pca
    Saved as PCA_test_has the full control.

--- pca_test_oop.py
+++ pca_test_oop.py
@@ -103,30 +103,30 @@ counter  = 1
 while counter:
     
     
-    #L1.extend_limb(M_starfish)
-    #L2.extend_limb(M_starfish)
-    #L3.extend_limb(M_starfish)
-    #L4.extend_limb(M_starfish)
-    #L5.extend_limb(M_starfish)
+    L1.extend_limb(M_starfish)
+    L2.extend_limb(M_starfish)
+    L3.extend_limb(M_starfish)
+    L4.extend_limb(M_starfish)
+    L5.extend_limb(M_starfish)
     
-    #L1.engage_gecko(M_gecko)
-    #L2.engage_gecko(M_gecko)
-    #L3.engage_gecko(M_gecko)
-    #L4.engage_gecko(M_gecko)
-    #L5.engage_gecko(M_gecko)
+    L1.engage_gecko(M_gecko)
+    L2.engage_gecko(M_gecko)
+    L3.engage_gecko(M_gecko)
+    L4.engage_gecko(M_gecko)
+    L5.engage_gecko(M_gecko)
 
-    #L1.contract_limb(M_starfish)
-    #L2.contract_limb(M_starfish)
-    #L3.contract_limb(M_starfish)
-    #L4.contract_limb(M_starfish)
-    #L5.contract_limb(M_starfish)
+    L1.contract_limb(M_starfish)
+    L2.contract_limb(M_starfish)
+    L3.contract_limb(M_starfish)
+    L4.contract_limb(M_starfish)
+    L5.contract_limb(M_starfish)
 
     
-    #L1.disengage_gecko(M_gecko)
-    #L2.disengage_gecko(M_gecko)
-    #L3.disengage_gecko(M_gecko)
-    #L4.disengage_gecko(M_gecko)
-    #L5.disengage_gecko(M_gecko)
+    L1.disengage_gecko(M_gecko)
+    L2.disengage_gecko(M_gecko)
+    L3.disengage_gecko(M_gecko)
+    L4.disengage_gecko(M_gecko)
+    L5.disengage_gecko(M_gecko)
 
     
     
@@ -135,49 +135,3 @@ while counter:
     break
 
 #pwm.set_pwm(15,0,0)
-def Move_to_target(start,end):
-    dist = abs(end - start)
-    steps = (dist//1.2) + 1
-    print(steps)
-    
-    while steps:
-        
-        # Engage Lower Limb
-        L3.engage_gecko(M_gecko)
-        L4.engage_gecko(M_gecko)
-        L5.engage_gecko(M_gecko)
-        
-        # Move Forward
-        L1.extend_limb(M_starfish)
-        L2.extend_limb(M_starfish)
-        
-        # Engage Forward Limbs
-        L1.engage_gecko(M_gecko)
-        L2.engage_gecko(M_gecko)
-        
-        # Disengage Back Limbs
-        L3.disengage_gecko(M_gecko)
-        L4.disengage_gecko(M_gecko)
-        L5.disengage_gecko(M_gecko)
-        
-        # Shift Forward -> Compress back Limbs
-        L3.contract_limb(M_starfish)
-        L4.contract_limb(M_starfish)
-        L5.contract_limb(M_starfish)
-        
-        # Compress Forward Limbs Just for stall
-        L1.contract_limb(M_starfish)
-        L2.contract_limb(M_starfish)
-        
-        #Disengage Gecko Patch
-        L1.disengage_gecko(M_gecko)
-        L2.disengage_gecko(M_gecko)
-        
-        if steps%100 == 0:
-            print('Steps left',steps-1)
-        steps -=1
-    
-    print('Motion complete')
-    return 1
-
-Move_to_target(0,6)
